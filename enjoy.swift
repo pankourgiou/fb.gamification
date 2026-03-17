d92b52348478d44a38721903ca5178f0b1060f0b
it all seems greek to me! <60981745+pankourgiou@users.noreply.github.com>
2026-03-17 10:20:46 +0200

    Create enjoy.swift

---
+++ enjoy.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+while true {
+    print("delay facebook and use the poem the marriage of heaven and hell and rock 'n roll/heavy metal lyrics so that we really play a rock 'n roll poetry game")
+    Thread.sleep(forTimeInterval: 1.0)
+}
